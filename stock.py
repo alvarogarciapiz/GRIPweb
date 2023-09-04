b59b734f0bfa689613291ca1f11275867d133f84
Álvaro García Pizarro <81751965+alvarogarciapiz@users.noreply.github.com>
2023-09-04 08:36:32 +0200

    Python script to update stock

---
+++ stock.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import json
+import time
+
+SHEET_ID = '1r8KnwTRDr5DlocXelGBzHcGcu_76dICN'
+SHEET_NAME = 'STOCKWEB'
+nombre_archivo = 'data.json'
+url = f'https://docs.google.com/spreadsheets/d/{SHEET_ID}/gviz/tq?tqx=out:csv&sheet={SHEET_NAME}'
+
+df = pd.read_csv(url)
+data_dict = df.to_dict(orient='records')
+
+with open(nombre_archivo, 'w') as archivo:
+    json.dump(data_dict, archivo, indent=4)
+print(f'El JSON se ha guardado en {nombre_archivo}')
