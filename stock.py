131e61c0b5cd5060b3f6cc549d1ef3026fc2eb3b
Álvaro García Pizarro <81751965+alvarogarciapiz@users.noreply.github.com>
2023-09-05 17:23:08 +0200

    Update stock.py

--- stock.py
+++ stock.py
@@ -11,4 +11,10 @@ data_dict = df.to_dict(orient='records')
 
 with open(nombre_archivo, 'w') as archivo:
     json.dump(data_dict, archivo, indent=4)
+
+# Imprime el resultado
+with open(nombre_archivo, 'r') as archivo:
+    contenido_json = archivo.read()
+    print(contenido_json)
+    
 print(f'El JSON se ha guardado en {nombre_archivo}')
